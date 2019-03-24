c9a7915e8ea6f91d16efa4bdcc8c52bf7585f43a
Edward Knight <edw@rdknig.ht>
2019-03-24 20:50:35 +0000

    Generate weeds after the river in compose_image

--- cultivate/map.py
+++ cultivate/map.py
@@ -12,7 +12,7 @@ from cultivate.loader import get_bridge
 
 class Map:
     def __init__(self):
-        self.compose_image()
+        self.image = self.compose_image()
         self.map_view_x = WIDTH
         self.map_view_y = HEIGHT
         self.width = self.image.get_rect().width
@@ -22,18 +22,17 @@ class Map:
         self.up = 0
         self.down = self.height - HEIGHT
         self.move_amount = 50
-        self.generate_random_weeds()
         self.buildings = {"test building": TestBuilding(self.image)}
 
-
-    def compose_image(self):
-        self.image = get_grass(MAP_WIDTH, MAP_HEIGHT)
+    def compose_image(self) -> pygame.Surface:
+        image = get_grass(MAP_WIDTH, MAP_HEIGHT)
+        self.generate_random_weeds(image)
         river = get_river(MAP_HEIGHT)
-        self.image.blit(river, ((MAP_WIDTH / 2), 0))
+        image.blit(river, ((MAP_WIDTH / 2), 0))
         bridge = get_bridge()
         bridge_coords = [(((MAP_WIDTH / 2) + 2), (MAP_HEIGHT * 2 / 3)), (((MAP_WIDTH / 2) + 2), (MAP_HEIGHT / 3))]
         for coords in bridge_coords:
-            self.image.blit(bridge, coords)
+            image.blit(bridge, coords)
         dirt_path = get_dirt_path()
         dirt_path_coords = [(((MAP_WIDTH / 2) - 26), (MAP_HEIGHT * 2 / 3)),
                             (((MAP_WIDTH / 2) + 46), (MAP_HEIGHT * 2 / 3)),
@@ -41,14 +40,16 @@ class Map:
                             (((MAP_WIDTH / 2) + 46), (MAP_HEIGHT / 3))
                             ]
         for coords in dirt_path_coords:
-            self.image.blit(dirt_path, coords)
-        return self.image
+            image.blit(dirt_path, coords)
+        return image
 
-    def generate_random_weeds(self, count=100):
+    @staticmethod
+    def generate_random_weeds(surface: pygame.Surface, count=100):
+        """Randomly blit weeds onto {surface}."""
         weed = get_weed()
         locations = [(random.randrange(0, MAP_WIDTH), random.randrange(0, MAP_HEIGHT)) for _ in range(count)]
         for x, y in locations:
-            self.image.blit(weed, (x, y))
+            surface.blit(weed, (x, y))
 
     def update_map_view(self, key_pressed):
         if key_pressed[pygame.K_DOWN] or key_pressed[pygame.K_s]:
