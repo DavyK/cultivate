e266213ee0a7d8a884f11c31a4dd027dc69a5619
Davy Kavanagh <davy.kavanagh@intercom.io>
2019-03-24 17:32:23 +0000

    stuff

---
+++ cultivate/assets/dialogue/__init__.py
@@ -0,0 +1,52 @@
+class CultLeaderDialogue:
+
+    intro = """
+        Hello! Welcome to our little community. We are one big family here,
+        and we are so excited that you have decided to join us. We believe
+        that if everyone works together, the world will be bathed in light!
+        Thanks, K Byeeeeeee!
+    """
+
+    dig_holes = """
+        Hello {character_name}. I have a very important task for you. The dedication
+        ceremony of our new members is in 5 days. I need you to dig 6 holes, 6ft deep.
+        Thanks, K Byeeeeeee!
+    """
+
+    lemonade = """
+        Hello {character_name}. I need you to make
+        some lemonade for the dedication ceremony. The initiates will need some refreshments during the
+        ceremony. When you are done, you can leave it in the kitchen.
+        Thanks, K Byeeeeeee!
+    """
+
+    wash_robes = """
+        Hello {character_name}. I have another very important task for you. There are now clean white
+        robes, for the dedication ceremony, in TWO DAYS. I need you to wash and dry some robes for the
+        ceremony.
+        Thanks, K Byeeeeeee!
+    """
+
+    black_candles = """
+        Hello {character_name}. I have another very important task for you. The dedication ceremony requires
+        some very important black candles. I'll need you to make some.
+        Thanks, K Byeeeeeee!
+    """
+
+    prayer_sheets = """
+        Hello {character_name}. I have another very important task for you. Let's make sure everyone at the dedication
+        ceremony is reading for the same prayer sheet. I need you to spell check and then make copies of this prayer
+        sheet.
+        Thanks, K Byeeeeeee!
+    """
+
+
+
+
+
+
+class MainCharacterDialogue:
+
+    epiphany = 'Wait a second... Am I in a cult?'
+
+
