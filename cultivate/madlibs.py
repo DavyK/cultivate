26711b0f4bcbc8f3559586778e1980c6f0a84a7d
Edward Knight <edw@rdknig.ht>
2019-03-27 13:47:06 +0000

    Handle newlines
    Closes #54

--- cultivate/madlibs.py
+++ cultivate/madlibs.py
@@ -45,70 +45,81 @@ class Madlibs:
         # draw background
         pygame.draw.rect(surface, self.background_color, self.box_rect)
 
-        # calculate words and word_colors
-        unformatted_words = self.unformattted_prose.split(" ")
-        words = []
-        word_colors = []
+        # split prose into lines
+        unformatted_lines = self.unformattted_prose.splitlines()
+        lines = []
+        colors = []
         words_formatted_count = 0
-        for word in unformatted_words:
-            if "{" in word and "}" in word:
-                words.append(word.format_map(self.changed_words))
-                if self.selected_word_index == words_formatted_count:
-                    word_colors.append(self.text_editing_color)
+        for line in unformatted_lines:
+            # split each line into words and calculate word_colors
+            unformatted_words = line.split(" ")
+            words = []
+            word_colors = []
+            for word in unformatted_words:
+                if "{" in word and "}" in word:
+                    words.append(word.format_map(self.changed_words))
+                    if self.selected_word_index == words_formatted_count:
+                        word_colors.append(self.text_editing_color)
+                    else:
+                        word_colors.append(self.text_editable_color)
+                    words_formatted_count += 1
                 else:
-                    word_colors.append(self.text_editable_color)
-                words_formatted_count += 1
-            else:
-                words.append(word)
-                word_colors.append(self.text_color)
+                    words.append(word)
+                    word_colors.append(self.text_color)
+            lines.append(words)
+            colors.append(word_colors)
 
-        # draw words
-        self.draw_text(surface, words, word_colors)
+        # draw lines of words
+        self.draw_text(surface, lines, colors)
 
-    def draw_text(self, surface: pygame.Surface, words: typing.List[str],
-                  word_colors: typing.List[pygame.Color]) -> None:
+    def draw_text(self, surface: pygame.Surface,
+                  lines: typing.List[typing.List[str]],
+                  colors: typing.List[typing.List[pygame.Color]]) -> None:
         cursor_y = self.text_rect.top
 
         # get the height of the font
         font_height = self.font.size("Tg")[1]
 
-        while len(words) > 0:
-            # number of words on this line
-            i = 1
+        for words, word_colors in zip(lines, colors):
+            while len(words) > 0:
+                # number of words on this line
+                i = 1
 
-            # determine if the row of text will be outside our area
-            if cursor_y + font_height > self.text_rect.bottom:
-                # give up rendering text and stop where we are
-                logging.error("Could not fit all of the text onto screen")
-                break
+                # determine if the row of text will be outside our area
+                if cursor_y + font_height > self.text_rect.bottom:
+                    # give up rendering text and stop where we are
+                    logging.error("Could not fit all of the text onto screen")
+                    break
 
-            # try and fit more words on this line
-            while self.font.size(" ".join(words[:i + 1]))[0] < self.text_rect.width and i < len(words):
-                i += 1
+                # try and fit more words on this line
+                while self.font.size(" ".join(words[:i + 1]))[0] < self.text_rect.width and i < len(words):
+                    i += 1
 
-            # render the words
-            rendered_words = []
-            rendered_space = self.font.render(" ", True, self.text_color)
-            for word, word_color in zip(words[:i], word_colors[:i]):
-                if word[-1] not in string.ascii_lowercase:
-                    # always render punctuation at the end of a word in {self.text_color}
-                    rendered_words.append(self.font.render(word[:-1], True, word_color))
-                    rendered_words.append(self.font.render(word[-1], True, self.text_color))
-                else:
-                    rendered_words.append(self.font.render(word, True, word_color))
-                rendered_words.append(rendered_space)
+                # render the words
+                rendered_words = []
+                rendered_space = self.font.render(" ", True, self.text_color)
+                for word, word_color in zip(words[:i], word_colors[:i]):
+                    if word == "":
+                        continue
+                    if word[-1] not in string.ascii_lowercase:
+                        # always render punctuation at the end of a word in {self.text_color}
+                        rendered_words.append(self.font.render(word[:-1], True, word_color))
+                        rendered_words.append(self.font.render(word[-1], True, self.text_color))
+                    else:
+                        rendered_words.append(self.font.render(word, True, word_color))
+                    rendered_words.append(rendered_space)
 
-            # blit the rendered words to the surface
-            cursor_x = self.text_rect.left
-            for rendered_word in rendered_words:
-                surface.blit(rendered_word, (cursor_x, cursor_y))
-                cursor_x += rendered_word.get_width()
+                # blit the rendered words to the surface
+                cursor_x = self.text_rect.left
+                for rendered_word in rendered_words:
+                    surface.blit(rendered_word, (cursor_x, cursor_y))
+                    cursor_x += rendered_word.get_width()
 
-            cursor_y += font_height + 2
+                cursor_y += font_height + 2
 
-            # remove the text we just blitted
-            words = words[i:]
-            word_colors = word_colors[i:]
+                # remove the text we just blitted
+                words = words[i:]
+                word_colors = word_colors[i:]
 
     def handle_keypress(self, key) -> None:
         if key == pygame.K_TAB or key == pygame.K_RETURN:
