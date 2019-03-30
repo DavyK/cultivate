9bd3f1a62fe3919c0404d3d28b66520ce56a19bc
Edward Knight <edw@rdknig.ht>
2019-03-30 17:23:17 +0000

    Delete copypasta

--- cultivate/sprites/buildings/dorm.py
+++ cultivate/sprites/buildings/dorm.py
@@ -7,8 +7,8 @@ from cultivate.sprites.buildings import Building
 from cultivate.loader import get_floor, get_bed_sign, get_roof_small, get_walls, get_walls_edge, get_sideways_bed, get_bed
 
 
-class HorizontalDorm(Building):
-    """ Dormitory"""
+class Dorm(Building):
+    """Empty dormitory."""
 
     @property
     def width(self) -> int:
@@ -34,7 +34,13 @@ class HorizontalDorm(Building):
         return get_bed_sign()
 
     def draw_items(self, map_background: pygame.Surface):
-        # items
+        pass
+
+
+class HorizontalDorm(Dorm):
+    """Dormitory with horizontal beds."""
+
+    def draw_items(self, map_background: pygame.Surface):
         bed_left = get_sideways_bed()
         bed_right = pygame.transform.flip(bed_left, True, False)
 
@@ -79,47 +85,13 @@ class HorizontalDorm(Building):
             ),
         ]
 
-
         self.impassables.add(impassable_beds)
 
 
-import typing
-
-import pygame
-
-from cultivate.sprites import UpdatableSprite
-from cultivate.sprites.buildings import Building
-from cultivate.loader import get_floor, get_library_sign, get_roof_small, get_walls, get_walls_edge, get_sideways_bed
-
-
-class VerticalDorm(Building):
-    """ Dormitory"""
-
-    @property
-    def width(self) -> int:
-        return 200
-
-    @property
-    def height(self) -> int:
-        return 200
-
-    def get_floor(self) -> pygame.Surface:
-        return get_floor(self.rect.w, self.rect.h)
-
-    def get_top_wall(self) -> pygame.Surface:
-        return get_walls(self.rect.w)
-
-    def get_side_wall(self) -> pygame.Surface:
-        return get_walls_edge(self.rect.h)
-
-    def get_roof(self) -> typing.Tuple[pygame.Surface, int]:
-        return get_roof_small(), 100
-
-    def get_sign(self) -> pygame.Surface:
-        return get_bed_sign()
+class VerticalDorm(Dorm):
+    """Dormitory with vertical beds."""
 
     def draw_items(self, map_background: pygame.Surface):
-        # items
         bed = get_bed()
 
         map_background.blit(bed, (self.rect.x + 25, self.rect.y + 50))
@@ -137,6 +109,5 @@ class VerticalDorm(Building):
                 )
             ),
         ]
-
 
         self.impassables.add(impassable_beds)
