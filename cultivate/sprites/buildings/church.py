4d2251355e578922245088aa9ccadd49a7aa8548
Glen Churchward <glenchurchward@gmail.com>
2019-03-30 22:39:27 +0000

    added impassable

--- cultivate/sprites/buildings/church.py
+++ cultivate/sprites/buildings/church.py
@@ -1,15 +1,17 @@
 import pygame
 from cultivate.loader import get_stone_cross_floor, get_stone_cross_wall, get_altar, get_pews, get_church_roof
 from cultivate import settings
+from cultivate.sprites import UpdatableSprite
 
 
 class Church:
     def __init__(self, map_background: pygame.Surface):
+
         self.rect = pygame.Rect(3000, 1500, 288, 544)
         self.floor = get_stone_cross_floor(self.rect.w, self.rect.h)
         self.walls = get_stone_cross_wall(288, 544)
         self.pews = get_pews()
-        self.altar = get_altar()
+        altar = get_altar()
         map_background.blit(self.floor, (self.rect.x, self.rect.y))
         map_background.blit(self.walls, (self.rect.x, self.rect.y))
         pew_coords = [(self.rect.x + 112, self.rect.y + (int((544 - 32) * 7 / 16) + 48)),
@@ -18,9 +20,13 @@ class Church:
                       (self.rect.x + 112, self.rect.y + (int((544 - 32) * 7 / 16) + 182))]
         for coord in pew_coords:
             map_background.blit(self.pews, coord)
-        map_background.blit(self.altar, ((self.rect.x + 128), (self.rect.y + (int((544 - 32) * 7 / 16) - 64))))
+        map_background.blit(altar, ((self.rect.x + 128), (self.rect.y + (int((544 - 32) * 7 / 16) - 64))))
         self.roof = get_church_roof()
+        impassable_altar = UpdatableSprite(
+            pygame.Rect(self.rect.x + 128, self.rect.y + (int((544 - 32) * 7 / 16) - 64),
+                        altar.get_rect().w, altar.get_rect().h))
 
+        self.impassables = pygame.sprite.Group(impassable_altar)
 
     def update(self, view_port: pygame.Rect):
         self.rect.x = 3000 - view_port.x
